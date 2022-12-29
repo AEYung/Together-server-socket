39c3a818fa57c10fc2a9fc268ae439d8a30e2c8d
jeayoung <k01066624566@gmail.com>
2022-12-29 17:31:29 +0900

    signalling + chatting

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+peerServer.on("connection", (client) =>{
+  console.log("connected ", client.getId)
+})
+
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
